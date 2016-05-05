63ea77ab7077f04e1ec601527de4a7acd2574088
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-05-05 14:55:58 -0700

    Traffic light logic prototype.

--- index.js
+++ index.js
@@ -19,3 +19,59 @@ app.get('/modify', function (req, res) {
 server.listen(port, hostname, function () {
   console.log('Listening on port %s. Open http://localhost:%s in browser.', port, port);
 });
+
+var lightBits = {
+  green: [0,0],
+  yellow: [0,1],
+  flashing: [1,0],
+  red: [1,1]
+};
+
+var createLight = function() {
+  return {
+    green: 10,
+    yellow: 2,
+    flashing: 4,
+    red: 2,
+    current: 'red'
+  };
+};
+
+var light = createLight();
+
+function runLights(light, currentStartingPin) {
+  setTimeout(function() {
+    console.log(currentStartingPin, light.current);
+    switch(light.current) {
+    case 'red':
+      // Switch to other light then flashing
+      currentStartingPin = currentStartingPin === 0 ? 2 : 0;
+      console.log('Current starting pin: ' + currentStartingPin);
+      light.current = 'flashing';
+      break;
+    case 'green':
+      // Switch to yellow
+      light.current = 'yellow';
+      break;
+    case 'yellow':
+      // Switch to red
+      light.current = 'red';
+      break;
+    case 'flashing':
+      // Switch to green
+      light.current = 'green';
+      break;
+    default:
+      console.log('wtf');
+    }
+    return runLights(light, currentStartingPin);
+  }, light[light.current] * 1000);
+}
+
+function switchLight(lightColor, currentStartingPin) {
+  lightSwitch(currentStartingPin, lightBits[lightColor][0]);
+  lightSwitch(currentStartingPin + 1, lightBits[lightColor][1]);
+}
+
+runLights(light, 0);
+
