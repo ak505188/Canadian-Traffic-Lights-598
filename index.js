bdb27bbefc909f941908be43e2f6db4d53a7f80b
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-05-05 11:53:47 -0700

    Get input on server from form.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var spawn = require('child_process').spawn;
-// spawn runs a command
-// commenting out because no point in creating a file every server start
-// Docs: https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
-var test = spawn('bash', ['./test.sh']);
 
 var isDeveloping = process.env.NODE_ENV !== 'production';
 var port = isDeveloping ? 3000 : process.env.PORT;
@@ -15,8 +10,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile('Traffic_Light_Timing_Manager.html', { root: __dirname });
-})
+});
+
+app.get('/modify', function (req, res) {
+  console.log(req.query);
+});
 
 server.listen(port, hostname, function () {
   console.log('Listening on port %s. Open http://localhost:%s in browser.', port, port);
-})
+});
